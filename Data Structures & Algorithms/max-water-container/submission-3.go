35f9903119895eee78f498eab767e87e2a59f2b3
Deepak <deepakgiri735055@gmail.com>
2026-04-18 02:19:47 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.go
@@ -0,0 +1,21 @@
+func maxArea(heights []int) int {
+    left := 0
+    right := len(heights) -1;
+    maxwater:=0
+    for(left != right){
+        if heights[left] > heights[right]{
+            water := (right - left) * heights[right]
+            right--
+            if water > maxwater{
+                maxwater = water
+            }
+        }else{
+            water := (right - left) * heights[left]
+            left++
+            if water > maxwater{
+                maxwater = water
+            }
+        }
+    }
+    return maxwater
+}
