ba79378faf77b25ef84877b966fd3f56532baafc
Deepak <deepakgiri735055@gmail.com>
2026-04-19 18:13:40 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> ck = new Stack<>();
+        for(char c : s.toCharArray())
+        {
+            if(c == '(' || c == '{' || c == '[')
+            {
+                ck.push(c);
+            }
+            else
+            {
+                if(ck.isEmpty()) return false;
+                char ch = ck.pop();
+                if(c == ')' && ch != '(' || c == '}' && ch != '{' || c == ']' && ch != '[')
+                {
+                    return false;
+                }
+            }
+        }
+        return ck.isEmpty();
+    }
+}
